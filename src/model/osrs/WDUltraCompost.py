917a760631e7adf3f400574dca72421d3e1bd2d5
Jared <hewentto@gmail.com>
2023-03-13 23:29:07 -0600

    updating api_m method names

--- src/model/osrs/WDUltraCompost.py
+++ src/model/osrs/WDUltraCompost.py
@@ -90,7 +90,7 @@ class OSRSWDUltraCompostMaker(OSRSBot):
             minutes_since_last_break = int((time.time() - self.last_break) / 60)
             seconds = int(time.time() - self.last_break) % 60
             percentage = (self.multiplier * .01)  # this is the percentage chance of a break
-            deposit_slots = self.api_m.get_inv_item_index(self.deposit_ids)
+            deposit_slots = self.api_m.get_inv_item_first_indice(self.deposit_ids)
             self.roll_chance_passed = False
 
             if len(self.api_m.get_inv_item_indices(ids.SUPERCOMPOST)) == 0:
@@ -345,7 +345,7 @@ class OSRSWDUltraCompostMaker(OSRSBot):
             None
         """
         # get unique items in inventory
-        unique_items = self.api_m.get_inv_item_index(self.withdraw_ids)
+        unique_items = self.api_m.get_inv_item_first_indice(self.withdraw_ids)
 
         # move mouse to each item and click
         for item in unique_items:
--- src/model/osrs/WDWoodcutting.py
+++ src/model/osrs/WDWoodcutting.py
@@ -130,7 +130,7 @@ class OSRSWDWoodcutting(OSRSBot):
             minutes_since_last_break = int((time.time() - self.last_break) / 60)
             seconds = int(time.time() - self.last_break) % 60
             percentage = (self.multiplier * .01)  # this is the percentage chance of a break
-            deposit_slots = self.api_m.get_inv_item_index(self.deposit_ids)
+            deposit_slots = self.api_m.get_inv_item_first_indice(self.deposit_ids)
             self.roll_chance_passed = False
             self.spec_energy = self.get_special_energy()
 
