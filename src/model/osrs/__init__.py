e94f7223ddd0c8dc8ac0b9b45c9ec25d99768d3e
Jared Denning <32106348+hewentto@users.noreply.github.com>
2023-04-01 14:19:45 -0600

    updates to woodcutting

--- src/model/osrs/__init__.py
+++ src/model/osrs/__init__.py
@@ -3,4 +3,5 @@ from .woodcutter import OSRSWoodcutter
 from .WillowsDad.WDWoodcutting import OSRSWDWoodcutting
 from .WillowsDad.WDUltraCompost import OSRSWDUltraCompostMaker
 from .WillowsDad.WDDegrimer import OSRSWDDegrimer
+from .WillowsDad.WDFishing import OSRSWDFishing
 from .WillowsDad.WillowsDad_bot import WillowsDadBot
