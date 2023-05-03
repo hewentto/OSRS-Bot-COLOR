0838e8b33fabe717abe56fedff036d71491e20c6
Jared Denning <32106348+hewentto@users.noreply.github.com>
2023-05-02 23:50:41 -0600

    Added thieving

--- src/model/osrs/__init__.py
+++ src/model/osrs/__init__.py
@@ -6,3 +6,4 @@ from .WillowsDad.WDDegrimer import OSRSWDDegrimer
 from .WillowsDad.WDFishing import OSRSWDFishing
 from .WillowsDad.WillowsDad_bot import WillowsDadBot
 from .WillowsDad.WDMining import OSRSWDMining
+from .WillowsDad.WDThieving import OSRSWDThieving
